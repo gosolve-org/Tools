0ed771b1e23ef544cc5b76ffe8ad600f0187c869
Tom Daniel <tom.daniel@ae.be>
2022-12-28 18:28:21 +0100

    Add ReadOnlyRepository functionality

---
+++ Tools.Common/Database/Repositories/Interfaces/IReadOnlyRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using GoSolve.Tools.Common.Database.Models;
+using GoSolve.Tools.Common.Database.Models.Interfaces;
+
+namespace GoSolve.Tools.Api.Database.Repositories.Interfaces;
+
+/// <summary>
+/// Interface for the read-only functionalities of a database repository.
+/// </summary>
+/// <typeparam name="TEntity">Type of the entity.</typeparam>
+/// <typeparam name="TId">Type of the id of the entity.</typeparam>
+public interface IReadOnlyRepository<TEntity, TId>
+{
+    /// <summary>
+    /// Gets an entity by its id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task<TEntity> GetById(TId id);
+
+    /// <summary>
+    /// Gets all entities.
+    /// </summary>
+    /// <returns></returns>
+    Task<IEnumerable<TEntity>> GetAll();
+
+    /// <summary>
+    /// Finds all entities which match a predicate.
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+}
---
+++ Tools.Common/Database/Repositories/ReadOnlyRepository.cs
@@ -0,0 +1,61 @@
+﻿using GoSolve.Tools.Api.Database.Repositories.Interfaces;
+using GoSolve.Tools.Common.Database.Models;
+using GoSolve.Tools.Common.Database.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace GoSolve.Tools.Common.Database.Repositories;
+
+/// <summary>
+/// Abstract class including the read-only functionality of a database repository.
+/// </summary>
+/// <typeparam name="TEntity">Type of the entity.</typeparam>
+/// <typeparam name="TId">Type of the id of the entity.</typeparam>
+/// <typeparam name="TDbContext">Type of the DbContext that is used for this entity.</typeparam>
+public abstract class ReadOnlyRepository<TEntity, TId, TDbContext> : IReadOnlyRepository<TEntity, TId>
+    where TEntity : BaseEntity<TId>
+    where TDbContext : BaseDbContext<TDbContext>
+{
+    /// <summary>
+    /// The database context.
+    /// </summary>
+    protected TDbContext Context { get; }
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="context"></param>
+    public ReadOnlyRepository(TDbContext context)
+    {
+        Context = context;
+    }
+
+    /// <summary>
+    /// Gets an entity by its id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public virtual async Task<TEntity> GetById(TId id)
+    {
+        return await Context.Set<TEntity>().FindAsync(id);
+    }
+
+    /// <summary>
+    /// Gets all entities.
+    /// </summary>
+    /// <returns></returns>
+    public virtual async Task<IEnumerable<TEntity>> GetAll()
+    {
+        return await Context.Set<TEntity>().ToListAsync();
+    }
+
+    /// <summary>
+    /// Finds all entities which match a predicate.
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    public virtual async Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await Context.Set<TEntity>().Where(predicate).ToListAsync();
+    }
+}
